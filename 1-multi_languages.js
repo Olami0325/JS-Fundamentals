0fd9dbec57744f96e4a2ade6a2840dcb0a93ea8f
oladimeji <oladimejisaeed@gmail.com>
2025-05-24 00:58:05 +0100

    task-3 on value argument completed

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,10 @@
+/*Write a script that prints 3 lines:
+
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+You must use console.log(...) to print all output*/
+
 
 console.log('C is fun');
 console.log('Python is cool');
--- 2-arguments.js
+++ 2-arguments.js
@@ -1,3 +1,13 @@
+/* Write a script that prints a message depending of the number of arguments passed:
+
+If no arguments are passed to the script, print “No argument”
+If only one argument is passed to the script, print “Argument found”
+Otherwise, print “Arguments found”
+You must use console.log(...) to print all output
+
+Reference: process.argv*/
+
+
 
 const args = process.argv.slice(2);
 if(args.length === 0){
