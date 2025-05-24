0d17ea89d01b51972e3d06ef372c734431585c54
oladimeji <oladimejisaeed@gmail.com>
2025-05-25 00:42:23 +0100

    changes made

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -5,7 +5,7 @@ const multiLang = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 let i = 0;
 let result = '';
-while(i < multiLang.length) {
+while (i < multiLang.length) {
     result += multiLang[i] + '\n'; i++
 }
 console.log(result.trim())
