58ca6a296d3ecfe0f5ed0202804c4538fe7fc97e
oladimeji <oladimejisaeed@gmail.com>
2025-05-24 01:50:40 +0100

    correction made

--- 4-concat.js
+++ 4-concat.js
@@ -1,15 +1,7 @@
 
 /* Write a script that prints two arguments passed to it, in the following format: “ is ”
 
-You must use console.log(...) to print all output
-
-guillaume@ubuntu:~/0x12$ node 4-concat.js c cool
-c is cool
-guillaume@ubuntu:~/0x12$ node 4-concat.js c
-c is undefined
-guillaume@ubuntu:~/0x12$ node 4-concat.js
-undefined is undefined
-guillaume@ubuntu:~/0x12$ */      
+You must use console.log(...) to print all output*/      
 
 const concat = process.argv.slice(2)
 
