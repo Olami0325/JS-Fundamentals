4b7b6dac1b18a628b78f8398f126cd28407bd0ee
oladimeji <oladimejisaeed@gmail.com>
2025-05-24 23:52:56 +0100

    correction made

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,12 +1,6 @@
 
 
-const multiLang = ["C is fun", "Python is  is cool", " JavaScript is amazing"];
-// let result = ''
-
-// for(let i = 0; i < multiLang.length; i++){
-//    result += multiLang[i] + '\n'
-// }
-//  console.log(result.trim())
+const multiLang = ["C is fun", "Python is cool", " JavaScript is amazing"];
 
 for(let i = 0; i < multiLang.length; i++){
     console.log(multiLang[i]);
