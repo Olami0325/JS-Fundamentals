653af865039361e6db74d7436786a59a37fb3d01
oladimeji <oladimejisaeed@gmail.com>
2025-05-24 18:41:37 +0100

    fix error on the conditional statement

--- 5-to_interger.js
+++ 5-to_interger.js
@@ -1,6 +1,6 @@
 
 const toInteger = process.argv.slice(2);
-if(toInteger[0]){
+if(!isNaN(parseInt(toInteger[0]))){
     console.log(`My number: ${parseInt(toInteger[0])}`)
 }else{
     console.log('Not a number')
