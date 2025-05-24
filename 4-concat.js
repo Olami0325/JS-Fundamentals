04db11acca7952327c0535b8003acdbd3a73e445
oladimeji <oladimejisaeed@gmail.com>
2025-05-24 02:07:06 +0100

    task-4 on arguments concat complted

--- 4-concat.js
+++ 4-concat.js
@@ -1,15 +1,17 @@
 
 /* Write a script that prints two arguments passed to it, in the following format: “ is ”
 
-You must use console.log(...) to print all output*/      
+You must use console.log(...) to print all output
+
+guillaume@ubuntu:~/0x12$ node 4-concat.js c cool
+c is cool
+guillaume@ubuntu:~/0x12$ node 4-concat.js c
+c is undefined
+guillaume@ubuntu:~/0x12$ node 4-concat.js
+undefined is undefined
+guillaume@ubuntu:~/0x12$ */      
 
 const concat = process.argv.slice(2)
+console.log(`${concat[0]} is ${concat[1]}`)
 
-if(concat[1]){
-    console.log('c is cool')
-}else if(concat[0]){
-    console.log('c is undefined')
-}else{
-    console.log('undefined is undefined')
-}
 
